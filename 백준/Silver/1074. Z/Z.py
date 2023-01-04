83b7295a92e6a6e3a8d4457b75b9b2c42f3b1004
Hyeon-Stone <79035672+Hyeon-Stone@users.noreply.github.com>
2023-01-04 13:18:03 +0900

    [Silver I] Title: Z, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def Z(N,r,c):
+    if N == 0 :
+        return 0
+    else:
+        return 2 * (r%2) + (c%2) + 4 * Z(N-1, r//2, c//2)
+
+N,r,c= map(int,input().split(' '))
+print(Z(N,r,c))
