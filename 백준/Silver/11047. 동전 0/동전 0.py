4310b08a90c00ecac623f69b517037613a7d6c71
Hyeon-Stone <hyun698207@gmail.com>
2023-05-10 20:51:38 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,20 +4,12 @@ input = sys.stdin.readline
 n, k = map(int, input().rstrip().split())
 coinList = [int(input()) for _ in range(n)]
 def coinCnt(coinList,n, k):
-    if k == 0:
-        return 0
-    s = 0
-    for i in range(n):
-        if coinList[i] > k:
-            s = i-1
-    if s == 0:
-        s = n-1
     cnt = 0
     while k != 0:
-        if k >= coinList[s]:
-            cnt += k // coinList[s]
-            k %= coinList[s]
+        if k >= coinList[n]:
+            cnt += k // coinList[n]
+            k %= coinList[n]
         else:
-            s -= 1
+            n -= 1
     return cnt
-print(coinCnt(coinList,n,k))
+print(coinCnt(coinList,n-1,k))
