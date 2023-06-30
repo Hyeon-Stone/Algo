e53a2ac54285bceffc10d002b4c849682b0d7eeb
Hyeon-Stone <hyun698207@gmail.com>
2023-06-30 22:49:00 +0900

    Update 경로 찾기.py

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -6,9 +6,9 @@ N = int(input())
 graph = [list(map(int,input().split())) for _ in range(N)]
 
 def dfs(N, graph):
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
+    for i in range(N): # 거치는 점
+        for j in range(N): # 시작하는 점
+            for k in range(N): # 끝 점
                 if graph[j][i] == 1 and graph[i][k] == 1:
                     graph[j][k] = 1
     return graph
