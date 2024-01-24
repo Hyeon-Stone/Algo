27a32393ce884269931a20d6d8729b19943eb7d1
Hyeon-Stone <hyun698207@gmail.com>
2024-01-25 00:42:22 +0900

    [level 2] Title: 타겟 넘버, Time: 1.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,6 +1,10 @@
 def solution(numbers, target):
-    if not numbers :
-        if target == 0:
-            return 1
-        else : return 0
-    return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+    queue = [0]
+    for num in numbers:
+        temp = []
+        while queue:
+            cal = queue.pop()
+            temp.append(cal+num)
+            temp.append(cal-num)
+        queue = temp
+    return queue.count(target)
