0a4f86592d866bdc6bc9b73b28494356ad8c5dcf
Hyeon-Stone <hyun698207@gmail.com>
2024-03-03 16:29:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.61 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        HashMap<String, Integer> completionMap = new HashMap<>();
+        for (String player : completion) {
+            completionMap.put(player, completionMap.getOrDefault(player, 0)+1);
+        }
+        
+        for (String player : participant) {
+            int cnt = completionMap.getOrDefault(player, 0);
+            if (cnt == 0) {
+                return player;
+            }
+            completionMap.put(player, cnt-1);
+        }
+        return null;
+    }
+}
