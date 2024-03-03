9cb92904ddc6d0fc3facba6a9dce571cc2153b8c
Hyeon-Stone <hyun698207@gmail.com>
2024-03-03 18:35:14 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> clothsMap = new HashMap<>();
+        for (String[] cloth : clothes) {
+            clothsMap.put(cloth[1], clothsMap.getOrDefault(cloth[1], 0)+1);
+        }
+        
+        Integer answer = 1;
+        for(Integer value : clothsMap.values()) {
+			answer *= value +1;
+		}
+        return answer -1;
+    }
+}
