cc0f34c92e3756ed193b9c858ca3105dad163f84
Hyeon-Stone <hyun698207@gmail.com>
2024-03-23 00:01:55 +0900

    [level 3] Title: 정수 삼각형, Time: 20.73 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 import java.util.Arrays;
 class Solution {
     public int solution(int[][] triangle) {        
-        return buttomUp(triangle);
+        return topDown(triangle);
     }
     
     private int buttomUp(int[][] triangle) {
@@ -11,7 +11,21 @@ class Solution {
                 triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
             }
         }
-        
         return triangle[0][0];
+    }
+    
+    private int topDown(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j ++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i-1][j];
+                } else if(j == triangle[i].length-1) {
+                    triangle[i][j] += triangle[i-1][j-1];
+                } else {
+                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+            }
+        }        
+        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
     }
 }
