2a6da1eca6108962b64925f56239e44f152301f2
Hyeon-Stone <hyun698207@gmail.com>
2024-03-22 20:47:09 +0900

    [level 2] Title: 기능개발, Time: 1.92 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 66.6 MB, 시간: 1.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 20:35:39
+2024년 03월 22일 20:47:08
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> days = new ArrayList<>();
+        for (int i = 0; i < progresses.length; i++) {
+            Integer day = (100 - progresses[i]) / speeds[i];
+            if (((100 - progresses[i]) % speeds[i]) != 0) {
+                day += 1 ;
+            }
+            //Integer day = (int) Math.ceil((double)((100 - progresses[i]) / speeds[i]));
+            
+            Integer len = days.size();
+            if (len == 0 || (Collections.max(days) >= day)) {
+                days.add(day);
+            } else {
+                answer.add(len);
+                days.clear();
+                days.add(day);
+            }
+        }
+        
+        if (days.size() != 0) {
+            answer.add(days.size());
+        }
+        return answer.stream().mapToInt(n -> n).toArray();
+    }
+}
