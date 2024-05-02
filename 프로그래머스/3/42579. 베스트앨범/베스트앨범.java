5cb4aa77a545d86f6de95a9e3d3af61ac8853d70
Hyeon-Stone <hyun698207@gmail.com>
2024-05-02 23:06:06 +0900

    [level 3] Title: 베스트앨범, Time: 2.79 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,26 +1,28 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        Map<String, Map<Integer, Integer>> playMap = new HashMap<>();
+        HashMap<String, HashMap<Integer, Integer>> playsPerGenreMap = new HashMap<>();
+        HashMap<String, Integer> totalPlayCountMap = new HashMap<>();
         
-        for (Integer index = 0; index < genres.length; index++) {
-            String genre = genres[index];
-            Map<Integer, Integer> playIndexMap = playMap.getOrDefault(genre, new HashMap<>());
-            playIndexMap.put(index, plays[index]);
-            playMap.put(genre, playIndexMap);
+        for (int i = 0; i < plays.length; i++) {
+            String genre = genres[i];
+            Integer play = plays[i];
+            HashMap<Integer, Integer> playIndexMap = playsPerGenreMap.getOrDefault(genre, new HashMap<>());
+            playIndexMap.put(i, play);
+            playsPerGenreMap.put(genre, playIndexMap);
+            
+            totalPlayCountMap.put(genre, totalPlayCountMap.getOrDefault(genre, 0) + play);
         }
         
-        Map<String, Integer> totalPalysMap = new HashMap<>();
-        for (String play : playMap.keySet()) {
-            totalPalysMap.put(play, playMap.get(play).values().stream().mapToInt(n->n).sum());
-        }
+        List<String> playMapKeyList = new ArrayList<>(playsPerGenreMap.keySet());
+        playMapKeyList.sort((o1, o2) -> (totalPlayCountMap.get(o2).compareTo(totalPlayCountMap.get(o1))));
         
-        List<String> keySetList = new ArrayList<>(playMap.keySet());
-        keySetList.sort((o1, o2) -> (totalPalysMap.get(o2).compareTo(totalPalysMap.get(o1))));
-        
-        for (String key : keySetList) {
-            Map<Integer, Integer> lastPlayIndexMap = playMap.get(key);
+        for (String key : playMapKeyList) {
+            HashMap<Integer, Integer> lastPlayIndexMap = playsPerGenreMap.get(key);
             List<Integer> indexList = new ArrayList<>(lastPlayIndexMap.keySet());
             
             if (indexList.size() == 1) {
@@ -29,11 +31,10 @@ class Solution {
             }
             
             indexList.sort((o1, o2) -> (lastPlayIndexMap.get(o2).compareTo(lastPlayIndexMap.get(o1))));
-        
+            
             answer.add(indexList.get(0));
             answer.add(indexList.get(1));
         }
-        
         return answer.stream().mapToInt(n -> n).toArray();
     }
 }
