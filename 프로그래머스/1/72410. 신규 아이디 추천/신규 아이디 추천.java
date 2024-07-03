c1df29a71a8afe093f7dbe36175c6b91e39d9e5d
Hyeon-Stone <hyun698207@gmail.com>
2024-07-03 15:39:13 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.80 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,7 +3,7 @@ class Solution {
         String answer = "";
         new_id = new_id.toLowerCase();
         new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
-        new_id = new_id.replaceAll("\\.+", ".");
+        new_id = new_id.replaceAll("\\.{2,}", ".");
         new_id = new_id.replaceAll("^\\.|\\.+$", "");
         new_id = new_id.isEmpty() ? "a" : new_id;
         new_id = new_id.length() >= 16 ? new_id.substring(0, 15).replaceAll("\\.+$", "") : new_id;
