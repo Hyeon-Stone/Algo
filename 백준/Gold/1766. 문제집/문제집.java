264b24debb0df7612e582d74fcf7ea94d9469349
Hyeon-Stone <hyun698207@gmail.com>
2024-07-16 23:33:59 +0900

    [Gold II] Title: 문제집, Time: 804 ms, Memory: 60332 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static Map<Integer, List<Integer>> graph = new HashMap<>();
+    private static int[] cntList;
+    private static int n;
+    private static int m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        cntList = new int[n+1];
+
+        // 입력
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            List<Integer> aMap = graph.getOrDefault(a, new ArrayList<>());
+            aMap.add(b);
+            graph.put(a, aMap);
+            cntList[b]++;
+        }
+
+        toplogicalSort();
+    }
+
+    private static void toplogicalSort() {
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 1; i <n+1; i++) {
+            if (cntList[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            Integer node = queue.poll();
+            System.out.print(node + " ");
+
+            for (Integer next : graph.getOrDefault(node, new ArrayList<>())) {
+                cntList[next]--;
+                if (cntList[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+
+}
