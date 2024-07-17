588a3b0670cfe1312dc2216f33542f28afac5f05
Hyeon-Stone <hyun698207@gmail.com>
2024-07-17 21:20:50 +0900

    [Silver II] Title: N과 M (11), Time: 396 ms, Memory: 34520 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static int n;
+    private static int m;
+    private static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        permutation(new int[m], 0);
+        System.out.println(sb);
+    }
+
+    private static void permutation(int[] out, int depth) {
+        if (depth == m) {
+            for (int a : out) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int now = arr[i];
+            if (before != now) {
+                out[depth] = arr[i];
+                before = now;
+                permutation(out, depth+1);
+            }
+
+        }
+    }
+
+}
